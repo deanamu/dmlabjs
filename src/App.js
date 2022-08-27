6c5876e91b337d07fc982cd568fedb07b69b5c39
finoriko <finoriko@hotmail.com>
2022-08-27 14:54:38 +0900

    add dmlab jsversion

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Main />}></Route>
+					<Route path="/product/*" element={<Product />}></Route>
+					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+					<Route path="*" element={<NotFound />}></Route>
+				</Routes>
+			</BrowserRouter>
     </div>
   );
 }
